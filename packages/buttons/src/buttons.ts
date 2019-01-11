7c7447251e850ef8e68ec10f82365ea19cdee856
Ben Keen <ben.keen@gmail.com>
2019-01-10 16:06:35 -0800

    this is not fun

--- packages/buttons/src/buttons.ts
+++ packages/buttons/src/buttons.ts
@@ -5,19 +5,4 @@ export class Buttons {
     set(id) {
 
     }
-    another1(id) {
-
-    }
-    another3(id) {
-
-    }
-    another4(id) {
-        return 'sigh';
-    }
-    another5() {
-
-    }
-    ugh () {
-        return 0;
-    }
 }
